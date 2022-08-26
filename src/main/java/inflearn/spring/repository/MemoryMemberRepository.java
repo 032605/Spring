835a9861c774a793ae949456787724c49f8f4b28
032605 <032605@naver.com>
2022-08-26 23:37:16 +0900

    MemberService & MemberServiceTest (DI)

--- src/main/java/inflearn/spring/repository/MemoryMemberRepository.java
+++ src/main/java/inflearn/spring/repository/MemoryMemberRepository.java
@@ -8,6 +8,7 @@ public class MemoryMemberRepository implements MemeberRepository{
 
     /**
      * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+     * static이므로 인스턴스에 붙는게 아니고 class에 붙음. 만약에 static이 아니면 인스턴스에서 다른 db에 붙어서 문제.
      */
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
