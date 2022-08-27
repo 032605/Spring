ee2152de335c3bb6b323af2fcea58ef8302469e0
032605 <032605@naver.com>
2022-08-28 00:16:50 +0900

    Spring mvc - join&list

--- src/main/java/inflearn/spring/controller/MemberController.java
+++ src/main/java/inflearn/spring/controller/MemberController.java
@@ -1,9 +1,16 @@
 package inflearn.spring.controller;
 
+import inflearn.spring.domain.Member;
 import inflearn.spring.repository.MemberRepository;
 import inflearn.spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -14,4 +21,27 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMember();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
